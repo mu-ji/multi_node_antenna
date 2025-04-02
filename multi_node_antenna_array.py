bfa5f44a1ee2df85500d4045160a8c872c91b6a4
mu-ji <1142260696@qq.com>
2025-04-02 16:39:49 +0800

    add figure label

--- multi_node_antenna_array.py
+++ multi_node_antenna_array.py
@@ -25,8 +25,8 @@ axs[1].plot(np.arctan2(rx1_packet2_Q[1], rx1_packet2_I[1]) - np.arctan2(rx2_pack
 plt.show()
 
 
-data1 = np.load('100_data_1.npz')
-data2 = np.load('100_data_2.npz')
+data1 = np.load('1000_data_1.npz')
+data2 = np.load('1000_data_2.npz')
 
 rx1_packet1_I = data1['packet_1_I_data']
 rx1_packet1_Q = data1['packet_1_Q_data']
@@ -58,13 +58,13 @@ def compensate_phase_diff(phase_diff):
     return phase_diff
 
 diff_list = []
-for i in range(100):
+for i in range(1000):
     packet1_phase_diff = np.arctan2(rx1_packet1_Q[i], rx1_packet1_I[i]) - np.arctan2(rx2_packet1_Q[i], rx2_packet1_I[i])
     packet2_phase_diff = np.arctan2(rx1_packet2_Q[i], rx1_packet2_I[i]) - np.arctan2(rx2_packet2_Q[i], rx2_packet2_I[i])
 
-    plt.plot(packet1_phase_diff, marker='.')
-    plt.plot(packet2_phase_diff, marker='.')
-    plt.show()
+    # plt.plot(packet1_phase_diff, marker='.')
+    # plt.plot(packet2_phase_diff, marker='.')
+    # plt.show()
 
     packet1_phase_diff = compensate_phase_diff(packet1_phase_diff)
     packet2_phase_diff = compensate_phase_diff(packet2_phase_diff)
@@ -89,6 +89,8 @@ for i in range(100):
 #         diff_list[i] = diff_list[i] - 2*np.pi
 
 plt.hist(diff_list,100)
+plt.ylabel('count')
+plt.xlabel('two packet phase diff')
 plt.show()
 
 print(diff_list)
